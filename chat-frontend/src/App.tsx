6920334fa8260a32eace46f47ffd80e1127f973b
glassesandice <glassesandice@gmail.com>
2026-05-09 15:19:09 +0300

    semi-randomly pick color based on name

--- chat-frontend/src/App.tsx
+++ chat-frontend/src/App.tsx
@@ -1,6 +1,5 @@
-import "./App.css";
-
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import "./App.css";
 
 import {
   MainContainer,
@@ -13,27 +12,50 @@ import { useState } from "react";
 
 interface Message {
   text: string;
-  sender?: string;
+  sender: string;
 }
 
-function mergeSenders(messages: Message[]): Message[] {
+interface MessageMerged extends Message {
+  showSender: boolean;
+}
+
+function mergeSenders(messages: Message[]): MessageMerged[] {
   if (messages.length === 0) {
-    return messages;
+    return [];
   }
 
-  const result = [messages[0]];
-  let previousMessage = result[0];
+  const result = [{ ...messages[0], showSender: true }];
+  let previousMessage = messages[0];
   for (const message of messages.slice(1)) {
     if (message.sender == previousMessage.sender) {
       result.push({
-        text: message.text,
+        ...message,
+        showSender: false,
       });
     } else {
-      result.push(message);
+      result.push({
+        ...message,
+        showSender: true,
+      });
     }
     previousMessage = message;
   }
   return result;
+}
+
+const hashCode = function (s: string) {
+  return s.split("").reduce(function (a, b) {
+    a = (a << 5) - a + b.charCodeAt(0);
+    return a & a;
+  }, 0);
+};
+
+function pickMessageColor(name: string): string {
+  const nameHash = hashCode(name);
+  // experimentally inferred to give lt3_liz purple color
+  const angleBase = 270;
+  const angle = (angleBase + nameHash) % 360;
+  return `hsl(${angle} 30% 70%)`;
 }
 
 const Chat = ({
@@ -48,16 +70,23 @@ const Chat = ({
       <MainContainer>
         <ChatContainer>
           <MessageList>
-            {mergeSenders(messages).map((message) => (
+            {mergeSenders(messages).map((message, idx) => (
               <Message
+                key={`message-${idx}`}
                 type="text"
                 model={{
                   message: message.text,
                   direction: "outgoing",
                   position: "last",
                 }}
+                className="custom-color-message"
+                // @ts-expect-error style is defined is custom css based on var
+                style={{ "--bubble-color": pickMessageColor(message.sender) }}
               >
-                <Message.Header>{message.sender}</Message.Header>
+                <Message.Header>
+                  {message.showSender ? message.sender : null}
+                </Message.Header>
+                <Message.TextContent>{message.text}</Message.TextContent>
               </Message>
             ))}
           </MessageList>
